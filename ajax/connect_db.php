ccd62760ab0e829d194903b3e175c777c993a8e0
IcarusKenny <72690010+IcarusKenny@users.noreply.github.com>
2021-10-01 15:58:11 +0500

    Add files via upload

--- ajax/connect_db.php
+++ ajax/connect_db.php
@@ -2,9 +2,9 @@
     require_once('libs/rb-mysql.php');
     //R::setup( 'mysql:host=yvu4xahse0smimsc.chr7pe7iynqr.eu-west-1.rds.amazonaws.com; dbname=k9zsbj71aksi2up5','ixc1my3frytj29kr', 'zc6mpj1c9fzvgzca');
 
-    //echo R::setup('mysql:host=localhost;dbname=test_esoft','new_root', '') ? 'true' : 'false';
+    R::setup('mysql:host=localhost;dbname=esoft','francesco', 'some_pass');
 
-    R::setup( 'mysql:host=localhost;dbname=test_esoft','new_root', '');
+    //R::setup( 'mysql:host=185.103.109.52;dbname=test_esoft','root', '');
 
     $listOfTables = R::inspect();
 
