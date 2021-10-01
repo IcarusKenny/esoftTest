07d5e6687aeca1d6af7fdc2a458211d47035a9fb
IcarusKenny <72690010+IcarusKenny@users.noreply.github.com>
2021-10-01 12:47:21 +0500

    Add files via upload

--- ajax/connect_db.php
+++ ajax/connect_db.php
@@ -1,4 +1,4 @@
 <?php
     require_once('libs/rb-mysql.php');
-    R::setup( 'mysql:host=yvu4xahse0smimsc.chr7pe7iynqr.eu-west-1.rds.amazonaws.com	;dbname=k9zsbj71aksi2up5','ixc1my3frytj29kr', 'zc6mpj1c9fzvgzca');
+    R::setup( 'mysql:host=yvu4xahse0smimsc.chr7pe7iynqr.eu-west-1.rds.amazonaws.com; dbname=k9zsbj71aksi2up5','ixc1my3frytj29kr', 'zc6mpj1c9fzvgzca');
 ?>
