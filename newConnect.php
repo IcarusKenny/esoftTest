e2ee90cb3b9f932e7d811296ebe71c9fdd9499e8
IcarusKenny <72690010+IcarusKenny@users.noreply.github.com>
2021-10-01 15:53:29 +0500

    Add files via upload

--- newConnect.php
+++ newConnect.php
@@ -2,9 +2,10 @@
     $servername = "localhost";
     $username = "francesco";
     $password = "some_pass";
+    $db = "esoft";
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password, $db);
 
     // Check connection
     if ($conn->connect_error) {
